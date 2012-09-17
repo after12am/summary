3dcc61f8701647d8fcebb7afe4e9420277e6e1b0
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-09-17 14:00:16 +0900

    incorporating exception handling

--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 def main():
     import urllib
@@ -5,31 +6,46 @@ def main():
     
     # japanese site
     
-    uri = 'http://www.gizmodo.jp/2012/09/post_10869.html'
-    print uri
-    print extract(urllib.urlopen(uri).read())['body'], '\n'
-    print extract_img(urllib.urlopen(uri).read()), '\n'
+    try:
+        uri = 'http://www.gizmodo.jp/2012/09/post_10869.html'
+        print uri, '\n'
+        print extract(urllib.urlopen(uri).read())['body'], '\n'
+        print extract_img(urllib.urlopen(uri).read()), '\n'
+    except:
+        print sys.exc_info()[0], sys.exc_info()[1]
     
-    uri = 'http://labs.cybozu.co.jp/blog/nakatani/2007/09/web_1.html'
-    print uri
-    print extract(urllib.urlopen(uri).read())['body'], '\n'
-    print extract_img(urllib.urlopen(uri).read()), '\n'
+    try:
+        uri = 'http://labs.cybozu.co.jp/blog/nakatani/2007/09/web_1.html'
+        print uri, '\n'
+        print extract(urllib.urlopen(uri).read())['body'], '\n'
+        print extract_img(urllib.urlopen(uri).read()), '\n'
+    except:
+        print sys.exc_info()[0], sys.exc_info()[1]
     
     # english site
     
-    uri = 'http://capturevision.wordpress.com/2009/08/05/music-visualizer-progress/'
-    print uri
-    print extract(urllib.urlopen(uri).read())['body'], '\n'
-    print extract_img(urllib.urlopen(uri).read()), '\n'
+    try:
+        uri = 'http://capturevision.wordpress.com/2009/08/05/music-visualizer-progress/'
+        print uri, '\n'
+        print extract(urllib.urlopen(uri).read())['body'], '\n'
+        print extract_img(urllib.urlopen(uri).read()), '\n'
+    except:
+        print sys.exc_info()[0], sys.exc_info()[1]
     
-    uri = 'http://www.dasprinzip.com/prinzipiell/2012/04/03/shaders-in-their-natural-habitat-episode-1/'
-    print uri
-    print extract(urllib.urlopen(uri).read())['body'], '\n'
-    print extract_img(urllib.urlopen(uri).read()), '\n'
+    try:
+        uri = 'http://www.dasprinzip.com/prinzipiell/2012/04/03/shaders-in-their-natural-habitat-episode-1/'
+        print uri, '\n'
+        print extract(urllib.urlopen(uri).read())['body'], '\n'
+        print extract_img(urllib.urlopen(uri).read()), '\n'
+    except:
+        print sys.exc_info()[0], sys.exc_info()[1]
     
-    uri = 'http://www.generatorx.no/20101217/abstrakt-abstrakt-jorinde-voigt/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+generatorx+%28Generator.x%3A+Generative+strategies+in+art+%26+design%29'
-    print uri
-    print extract(urllib.urlopen(uri).read())['body'], '\n'
-    print extract_img(urllib.urlopen(uri).read()), '\n'
+    try:
+        uri = 'http://www.generatorx.no/20101217/abstrakt-abstrakt-jorinde-voigt/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+generatorx+%28Generator.x%3A+Generative+strategies+in+art+%26+design%29'
+        print uri, '\n'
+        print extract(urllib.urlopen(uri).read())['body'], '\n'
+        print extract_img(urllib.urlopen(uri).read()), '\n'
+    except:
+        print sys.exc_info()[0], sys.exc_info()[1]
 
 if __name__ == '__main__': main()

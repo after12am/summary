a31389449892c3214da8efedfc01ef89e73e5252
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-11-25 13:48:51 +0900

    simplify test case.

--- src/test.py
+++ src/test.py
@@ -2,60 +2,28 @@
 import urllib, sys
 from extractor import extract
 
-def main():
-    
+urls = [
     # japanese site
-    
-    try:
-        uri = 'http://www.gizmodo.jp/2012/09/post_10869.html'
-        print uri, '\n'
-        res = extract(urllib.urlopen(uri).read())
-        print 'Title: ', res['title'], '\n'
-        print 'Content: ', res['body'], '\n'
-        print 'Images: ', res['img'], '\n'
-    except:
-        print sys.exc_info()[0], sys.exc_info()[1]
-    
-    try:
-        uri = 'http://labs.cybozu.co.jp/blog/nakatani/2007/09/web_1.html'
-        print uri, '\n'
-        res = extract(urllib.urlopen(uri).read())
-        print 'Title: ', res['title'], '\n'
-        print 'Content: ', res['body'], '\n'
-        print 'Images: ', res['img'], '\n'
-    except:
-        print sys.exc_info()[0], sys.exc_info()[1]
-    
+    'http://www.gizmodo.jp/2012/09/post_10869.html',
+    'http://labs.cybozu.co.jp/blog/nakatani/2007/09/web_1.html',
     # english site
-    
+    'http://capturevision.wordpress.com/2009/08/05/music-visualizer-progress/',
+    'http://www.dasprinzip.com/prinzipiell/2012/04/03/shaders-in-their-natural-habitat-episode-1/',
+    'http://www.generatorx.no/20101217/abstrakt-abstrakt-jorinde-voigt/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+generatorx+%28Generator.x%3A+Generative+strategies+in+art+%26+design%29'
+]
+
+def extract_test(url):
     try:
-        uri = 'http://capturevision.wordpress.com/2009/08/05/music-visualizer-progress/'
-        print uri, '\n'
-        res = extract(urllib.urlopen(uri).read())
+        print url, '\n'
+        res = extract(urllib.urlopen(url).read())
         print 'Title: ', res['title'], '\n'
         print 'Content: ', res['body'], '\n'
         print 'Images: ', res['img'], '\n'
     except:
         print sys.exc_info()[0], sys.exc_info()[1]
-    
-    try:
-        uri = 'http://www.dasprinzip.com/prinzipiell/2012/04/03/shaders-in-their-natural-habitat-episode-1/'
-        print uri, '\n'
-        res = extract(urllib.urlopen(uri).read())
-        print 'Title: ', res['title'], '\n'
-        print 'Content: ', res['body'], '\n'
-        print 'Images: ', res['img'], '\n'
-    except:
-        print sys.exc_info()[0], sys.exc_info()[1]
-    
-    try:
-        uri = 'http://www.generatorx.no/20101217/abstrakt-abstrakt-jorinde-voigt/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+generatorx+%28Generator.x%3A+Generative+strategies+in+art+%26+design%29'
-        print uri, '\n'
-        res = extract(urllib.urlopen(uri).read())
-        print 'Title: ', res['title'], '\n'
-        print 'Content: ', res['body'], '\n'
-        print 'Images: ', res['img'], '\n'
-    except:
-        print sys.exc_info()[0], sys.exc_info()[1]
+
+def main():
+    for url in urls:
+        extract_test(url)
 
 if __name__ == '__main__': main()

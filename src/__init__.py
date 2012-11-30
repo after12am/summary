5f25960b5f2213603d5e10aca43652c43a2717f3
satoshi okami <satoshi@okami-satoshi-no-MacBook-Pro.local>
2012-11-30 22:01:47 +0900

    remove bug

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,3 @@
 from extractor import is_collection_of_links
 from extractor import not_body_rate
 from extractor import extract
-from extractor import extract_img
-from extractor import not_body_rate
